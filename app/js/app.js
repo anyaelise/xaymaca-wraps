1e7eb4d6821b7e6458e667e82f1fd4f4b65e6723
Anya Marshall <anya.elise.marshall@gmail.com>
2013-12-29 17:47:38 -0500

    Start structuring Angular modules

--- app/js/app.js
+++ app/js/app.js
@@ -1,15 +1,15 @@
 'use strict';
 
 
-// Declare app level module which depends on filters, and services
-angular.module('myApp', [
+// Declare app level module which depends on controllers, directives, filters, and services
+var xaymacaWraps = angular.module('xaymacaWraps', [
   'ngRoute',
-  'myApp.filters',
-  'myApp.services',
-  'myApp.directives',
-  'myApp.controllers'
-]).
-config(['$routeProvider', function($routeProvider) {
+  'xaymacaWraps.controllers',
+  'xaymacaWraps.directives'
+]);
+
+xaymacaWraps.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/', {templateUrl: 'partials/landing.html', controller: 'LandingController'});
   $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
   $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
   $routeProvider.otherwise({redirectTo: '/view1'});
