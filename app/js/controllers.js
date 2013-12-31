71182d1dd0471e0e32e08d1905ba3bec917165f2
Anya Marshall <anya.elise.marshall@gmail.com>
2013-12-30 19:10:27 -0500

    Lay out landing page

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -13,4 +13,23 @@ angular.module('xaymacaWraps.controllers', [])
                         { name: "YouTube", icon: "img/youtube.png", loc: "http://www.youtube.com/WrapXaymaca" }
                     ];
 
-  });
+  })
+.controller('MenuCtrl', function($scope) {
+    $scope.menu =   [   { name: "Home", loc: "#/"},
+                        { name: "Buy Body Wraps", loc: "http://wrapxaymaca.myitworks.com/shop/product/111/" },
+                        { name: "Start Your Own Business", loc: "#/biz" },
+                        { name: "About Me", loc: "#/about" },
+                        { name: "Before & After", loc: "#/before" },
+                        { name: "Contact", loc: "#/contact"}
+                    ];
+})
+.controller('ChecklistCtrl', function($scope) {
+    $scope.checklist =  [   { text: "Instant results" },
+                            { text: "Versatile - can be used anywhere from the neck to the toes!" },
+                            { text: "Tones, tightens and firms" },
+                            { text: "Continuous results over 72 hours" },
+                            { text: "Results last anywhere from 2 to 6 months" },
+                            { text: "All-natural active ingredients" },
+                            { text: "Convenient - done in the comfort of your home"}
+                        ];
+});
