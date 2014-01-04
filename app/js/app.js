38d2abb58866e80231dc89e05e288805a5d05e7c
Anya Marshall <anya.elise.marshall@gmail.com>
2014-01-04 00:50:07 -0500

    Lay out how to page

--- app/js/app.js
+++ app/js/app.js
@@ -12,5 +12,7 @@ xaymacaWraps.config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/before', {templateUrl: 'partials/before.html', controller: 'MainCtrl'});
   $routeProvider.when('/buy', {templateUrl: 'partials/buy.html', controller: 'MainCtrl'});
   $routeProvider.when('/biz', {templateUrl: 'partials/biz.html', controller: 'MainCtrl'});
+  $routeProvider.when('/howto', {templateUrl: 'partials/howto.html', controller: 'MainCtrl'});
+  $routeProvider.when('/contact', {templateUrl: 'partials/contact.html', controller: 'MainCtrl'});
   $routeProvider.otherwise({redirectTo: '/'});
 }]);
